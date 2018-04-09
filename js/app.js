6380275cabf826371f60795d124f76b9975917a3
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-04-09 15:41:33 +0100

    created basic controllers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(['routeProvider', function($routeProvider) {
+myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/login', {
             templateURL: 'views/login.html',
