0f00e725d66f40d8236d5af9ad361464f24cb775
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-04-09 16:47:15 +0100

    added firebase registration to controller

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,11 +1,18 @@
-myApp.controller('RegistrationController', ['$scope', function($scope) {
+myApp.controller('RegistrationController', ['$scope', '$firebase', '$firebaseAuth', function($scope, $firebase, $firebaseAuth) {
+
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
 
     $scope.login = function() {
         $scope.message = "Welcome " + $scope.user.email;
     };
 
     $scope.register = function() {
-        $scope.message = "Welcome " + $scope.user.firstname;
-    };
 
-}]);
+        auth.$createUserWithEmailAndPassword($scope.user.email, $scope.user.password).then(function(regUser) {
+            $scope.message = "Welcome " + $scope.user.firstname + ", Thanks for registering";
+        }).catch(function(error) {
+            $scope.message = error.message;
+        }); // creating a user w/ email and password
+    }; // register
+}]); // controller
