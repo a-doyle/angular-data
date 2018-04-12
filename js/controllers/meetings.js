01ae1e97bbc38a5f60deb9700eb39bf3de184577
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-04-12 11:16:24 +0100

    added second card to display meeting list
    also added functionality to delete meetings

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -1,3 +1,28 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
-    $scope.message = "Success!!!";
+myApp.controller('MeetingsController', '$firebaseAuth', '$firebaseArray', ['$scope', function($scope, $firebaseAuth, $firebaseArray) {
+
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+
+    auth.$onAuthStateChanged(function(authUser) {
+        if (authUser) {
+           var meetingsRef = ref.child('users').child(authUser.uid).child('meetings');
+           var meetingsInfo = $firebaseArray(meetingsRef);
+
+           $scope.meetings = meetingsInfo;
+
+           $scope.addMeeting = function() {
+               meetingsInfo.$add({
+                   name: $scope.meetingname,
+                   data: firebase.database.ServerValue.TIMESTAMP
+               }).then(function() {
+                   $scope.meetingname='';
+               })
+           } // add meeting
+
+           $scope.deleteMeeting = function(key) {
+               meetingsInfo.$remove(key);
+           } // deleteMeeting
+
+        } // authUser
+    });
 }]);
